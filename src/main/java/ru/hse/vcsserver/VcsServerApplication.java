f1a42514ac952ee89a427b6a0b8a14f2ccca7773
David <davidnatro03@gmail.com>
2023-05-06 18:02:39 +0600

    initial

---
+++ src/main/java/ru/hse/vcsserver/VcsServerApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.vcsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcsServerApplication.class, args);
+	}
+
+}
